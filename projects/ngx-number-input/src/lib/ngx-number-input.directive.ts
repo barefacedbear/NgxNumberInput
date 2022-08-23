afd9549269ec9fb47cf30818ccb24e52713d9283
Avishek Datta Ray <avishek.ray@programmers.io>
2022-08-23 12:51:31 +0530

    modified number formating logic

--- projects/ngx-number-input/src/lib/ngx-number-input.directive.ts
+++ projects/ngx-number-input/src/lib/ngx-number-input.directive.ts
@@ -1,33 +1,36 @@
 import { Directive, ElementRef, HostListener, Input, Optional } from '@angular/core';
 import { NgControl } from '@angular/forms';
 
+/**
+ * @description Directive class to format the number entered by user
+ * @author Barefaced Bear
+*/
 @Directive({
     selector: '[ngxNumberInput]'
 })
 export class NgxNumberInputDirective {
 
-    @Input('ngxNumberInput') numberPattern = '';
-    /**
-     * Value before the decimal point specifies the number of digits before decimal and value after the decimal specifies the number of digits after decimal.
-     * Example: 7.3 (Before decimal 7 digits & 3 digits after decimal)
-     */
-
     constructor(private el: ElementRef, @Optional() private control: NgControl) { }
 
+    /**
+     * @description Value before the decimal point specifies the number of digits before decimal and value after the decimal specifies the number of digits after decimal.
+     * Example: 7.3 (Before decimal 7 digits & 3 digits after decimal)
+     * Possible type of patterns allowed: X, X.X
+    */
+    @Input('ngxNumberInput') numberPattern = '';
+
     @HostListener("keydown", ["$event"])
-    onKeyDown(event: KeyboardEvent) {
-        this.execute(this.el.nativeElement.value);
-    }
+    onKeyDown = (event: KeyboardEvent) => this.execute(this.el.nativeElement.value);
 
     @HostListener("paste", ["$event"])
-    onPaste(event: ClipboardEvent) {
-        this.execute(this.el.nativeElement.value);
-    }
+    onPaste = (event: ClipboardEvent) => this.execute(this.el.nativeElement.value);
 
-    private checkValue(value: string) {
-        let [length, precision] = this.numberPattern.split('.'),
-            regExpString = `^(\\+|\\-)?([\\d]{0,${+length}})((\\.{1})([\\d]{1,${+precision}})?)?$`;
-        return String(value).match(new RegExp(regExpString));
+    private checkValue(value: string): RegExpMatchArray {
+        let [length, scale] = this.numberPattern.split('.'), regExpString = `^(\\+|\\-)?([\\d]{0,${+length}})`,
+        checkPattern: RegExpMatchArray = null;
+        if (+scale > 0) { regExpString += `((\\.{1})([\\d]{1,${+scale}})?)` }
+        checkPattern = String(value).match(new RegExp(`${regExpString}?$`));
+        return checkPattern;
     }
 
     private execute(oldValue: string) {
@@ -39,5 +42,4 @@ export class NgxNumberInputDirective {
             }
         });
     }
-
 }
